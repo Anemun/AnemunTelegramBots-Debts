b4a0b078cca1557d77f6b7b3dbbb6be83b38a4fb
Anemun <anemun@gmail.com>
2018-08-17 14:22:22 +0300

    Initial commit - ver 1.1-20180817

---
+++ Main.py
@@ -0,0 +1,92 @@
+import telebot
+import databaseProvider
+from sys import argv
+from telebot import types
+from datetime import datetime
+
+TELEGRAM_BOT_TOKEN = argv[1]
+DATABASE_FILE = argv[2]
+
+version = "1.1-20180817"
+
+bot = telebot.TeleBot(TELEGRAM_BOT_TOKEN)
+
+databaseProvider.Init(DATABASE_FILE)
+hideMarkup = types.ReplyKeyboardRemove()
+
+
+@bot.message_handler(commands=["ver"])
+def showVersion(message):
+    reply = "текущая версия: {0}".format(version)
+    bot.send_message(chat_id=message.chat.id, text=reply)
+
+
+@bot.message_handler(commands=["history"])
+def showHistory(message):
+    databaseResult = databaseProvider.getHistory()
+    text = ""
+    for i in range(len(databaseResult)):
+        text += str(databaseResult[i])
+        text += "\n"
+    bot.send_message(chat_id=message.chat.id,
+                     text=text)
+
+
+@bot.message_handler(commands=["help"])
+def showHelp(message):
+    bot.send_message(chat_id=message.chat.id,
+                     text="Используйте фразу 'долг Кто Кому Сумма'\nНапример: 'долг Роман Пётр 500' - значит, Роман должен Петру 500р")
+
+
+@bot.message_handler(commands=["debts"])
+def showDebts(message):
+    databaseResult = databaseProvider.getDebts()
+    text = ""
+    for i in range(len(databaseResult)):
+        text += str(databaseResult[i])
+        text += "\n"
+    bot.send_message(chat_id=message.chat.id,
+                     text=text)
+
+
+@bot.message_handler(regexp='долг')
+def confirmDebt(message):
+    text = str.split(message.text, ' ')
+    if len(text) == 4:
+        keyboard = types.InlineKeyboardMarkup()
+        button_yes = types.InlineKeyboardButton(text="Да", callback_data="writeDebtYes")
+        button_no = types.InlineKeyboardButton(text="Нет", callback_data="writeDebtNo")
+        keyboard.add(button_yes, button_no)
+        text = str.format("Кто: {0}\n"
+                          "Кому: {1}\n"
+                          "Сколько: {2}\n"
+                          "Записать данный долг?", text[1], text[2], text[3])
+        bot.send_message(chat_id=message.chat.id,
+                         text=text, reply_markup=keyboard)
+    else:
+        pass
+
+
+@bot.callback_query_handler(func=lambda call: True)
+def callback_inline(call):
+    if call.message:
+        raw = str.split(call.message.text, '\n')
+        who = str.split(raw[0], ' ')[1]
+        toWhom = str.split(raw[1], ' ')[1]
+        debt = str.split(raw[2], ' ')[1]
+        if call.data == "writeDebtYes":
+            databaseProvider.writeToDatabase(who, toWhom, debt)
+            bot.edit_message_text(chat_id=call.message.chat.id,
+                                  message_id=call.message.message_id,
+                                  text="Записано в базу!")
+        if call.data == "writeDebtNo":
+            bot.delete_message(chat_id=call.message.chat.id,
+                               message_id=call.message.message_id)
+        name = call.message.chat.id
+        date = str(datetime.now().strftime('%d-%m-%Y %H:%M:%S'))
+        text = "{0} {1} {2}".format(who, toWhom, debt)
+        databaseProvider.writeHistory(name, date, text)
+
+
+if __name__ == '__main__':
+    bot.polling(none_stop=True)
---
+++ databaseProvider.py
@@ -0,0 +1,76 @@
+import sqlite3
+
+databaseFilename = ""
+
+def Init(database):
+    global databaseFilename
+    databaseFilename = database
+    createTableIfNotExist()
+
+def createTableIfNotExist():
+    conn = sqlite3.connect(databaseFilename)
+    cursor = conn.cursor()
+
+    cursor.execute(
+        'CREATE TABLE IF NOT EXISTS [debts] ('
+        '[Id] integer PRIMARY KEY NOT NULL, '
+        '[who] string, '
+        '[toWhom] string, '
+        '[debt] string);')
+
+    cursor.execute(
+        'CREATE TABLE IF NOT EXISTS [history] ('
+        '[Id] integer PRIMARY KEY NOT NULL, '
+        '[name] string, '
+        '[date] datetime, '
+        '[message] string);')
+
+    conn.commit()
+    conn.close()
+
+
+def writeToDatabase(who, toWhom, value):
+    """
+    проверяем, есть ли такая пара людей
+    если пары нет, то создаём её
+    если пара есть, то плюсуем значение
+    """
+    result = runQuery("SELECT * FROM debts WHERE who='{0}' AND toWhom='{1}'".format(who, toWhom))
+    if len(result) == 0:
+        runQuery("INSERT INTO debts (who, toWhom, debt) VALUES ('{0}', '{1}', '{2}')".format(who, toWhom, value))
+    else:
+        newValue = int(result[0][3]) + int(value)
+        runQuery("UPDATE debts SET debt = '{0}' WHERE Id={1}".format(newValue, result[0][0]))
+    maintainBase()
+
+
+def writeHistory(name, date, message):
+    runQuery("INSERT INTO history (name, date, message) VALUES ('{0}', '{1}', '{2}')".format(name, date, message))
+
+
+def getHistory():
+    result = runQuery("SELECT * FROM history LIMIT 100")    
+    return result
+
+
+def getDebts():
+    maintainBase()
+    result = runQuery("SELECT * FROM debts")
+    return result
+
+
+def maintainBase():
+    base = runQuery("SELECT * FROM debts")
+    for i in range(len(base)):
+        if base[i][3] == 0:
+            runQuery("DELETE FROM debts WHERE id='{0}'".format(base[i][0]))
+
+
+def runQuery(query):
+    conn = sqlite3.connect(databaseFilename)
+    cursor = conn.cursor()
+    cursor.execute(query)
+    result = cursor.fetchall()
+    conn.commit()
+    conn.close()
+    return result
