8f38f9ab0c75e7e7012a0eaa6b96cfa0fc3fe75c
Anemun <anemun@gmail.com>
2018-10-15 15:02:05 +0300

    Fixed history sort order. again.

--- AnemunDebtsBot.py
+++ AnemunDebtsBot.py
@@ -12,7 +12,7 @@ args=parser.parse_args()
 TELEGRAM_BOT_TOKEN = args.botToken
 DATABASE_FILE = args.databasePath
 
-version = "1.2.0-20180924"
+version = "1.2.1-20181015"
 
 bot = telebot.TeleBot(TELEGRAM_BOT_TOKEN)
 
